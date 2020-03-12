bdabc6ae7f067fee3d6477d3b1a0f1205251d5de
fotis tsakiris <footios76@gmail.com>
2020-03-12 16:24:32 +0200

    toolbar with icons added (EvolvingButton)

--- src/EvolvingButton.js
+++ src/EvolvingButton.js
@@ -1,26 +1,50 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View, Animated, TextInput, Dimensions, TouchableWithoutFeedback } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  TextInput,
+  KeyboardAvoidingView,
+  Dimensions,
+  TouchableWithoutFeedback
+} from "react-native";
 
 import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
 
 export default class EvolvingButton extends Component {
-
   state = {
     animation: new Animated.Value(0),
-    open: false,
+    open: false
   };
 
-  toggleTransform = () => {
-    
-  }
+  toggleTransform = () => {};
 
   render() {
     const { width } = Dimensions.get("window");
 
-
     return (
       <View style={styles.container}>
-
+        <KeyboardAvoidingView style={styles.center} behavior="padding">
+          <Animated.View style={styles.editor}>
+            {/* Wrapping bar */}
+            <View style={styles.bar}>
+              {/* Use Animated to adjust the opacity */}
+              <Animated.View style={[styles.toolbar]}>
+                <Icon name="format-bold" color="#FFF" size={20} />
+                <Icon name="format-italic" color="#FFF" size={20} />
+                <Icon name="format-underline" color="#FFF" size={20} />
+                <Icon name="format-list-bulleted" color="#FFF" size={20} />
+                <Icon name="format-list-numbers" color="#FFF" size={20} />
+                <View style={styles.rightInnerBar}>
+                  <Icon name="link" color="#FFF" size={20} />
+                  <Icon name="image" color="#FFF" size={20} />
+                  <Icon name="arrow-down-bold-box" color="#FFF" size={20} />
+                </View>
+              </Animated.View>
+            </View>
+          </Animated.View>
+        </KeyboardAvoidingView>
       </View>
     );
   }
@@ -30,19 +54,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   center: {
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   buttonText: {
-    color: "#FFF",
+    color: "#FFF"
     // fontSize: 28
   },
   editor: {
     borderWidth: 1,
-    borderColor: "rgba(0,0, 0, .1)",
+    borderColor: "rgba(0,0, 0, .1)"
     // shadowColor: "#000",
     // shadowOpacity: .05,
     // shadowRadius: 3,
@@ -51,31 +75,31 @@ const styles = StyleSheet.create({
   bar: {
     backgroundColor: "#2979FF",
     height: 50,
-    justifyContent: "center",
+    justifyContent: "center"
   },
   toolbar: {
     flexDirection: "row",
     paddingHorizontal: 10,
     alignItems: "center",
     justifyContent: "flex-start",
-    overflow: "hidden",
+    overflow: "hidden"
   },
   rightInnerBar: {
     flexDirection: "row",
     alignItems: "center",
     flex: 1,
-    justifyContent: "flex-end",
+    justifyContent: "flex-end"
   },
   lowerView: {
     height: 150,
-    overflow: "hidden",
+    overflow: "hidden"
   },
   input: {
     padding: 10,
-    fontSize: 20,
+    fontSize: 20
   },
   close: {
     color: "#2979FF",
-    marginTop: 10,
+    marginTop: 10
   }
 });
